b060c2c4282ab1b72568fb7f53c50cd9098c8b97
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-02-21 10:23:25 +0700

    user registration and login

---
+++ src/main/java/com/library_management_system_monolith/entity/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.library_management_system_monolith.entity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Untuk sementara, kita tidak menggunakan role
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail(); // Menggunakan email sebagai username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
